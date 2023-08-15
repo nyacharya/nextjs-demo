0eb43093535ab4d54c98f701b78c93622d620854
Nayan Acharya <nayanacharya9@gmail.com>
2023-08-14 21:50:03 -0400

    revert and fix temp all

--- src/app/welcome/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function WelcomeLayout() {
-  return(
-    <h2 className=" text-green-500 text-bold">
-      WElcome again
-    </h2>
-  )
-}
--- src/app/welcome/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Welcome(){
-  return (
-    <h2 class="text-2xl underline text-red-500">
-      Hello Welcome
-      <div> Hello here </div>
-    </h2>
-  )
-}
