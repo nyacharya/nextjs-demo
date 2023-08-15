bcd0cfbae04258554dc77bfd7159dd8a2c4109d9
Nayan Acharya <nayanacharya9@gmail.com>
2023-08-14 21:39:31 -0400

    adding tailwind

---
+++ src/app/welcome/layout.tsx
@@ -0,0 +1,7 @@
+export default function WelcomeLayout({ children }) {
+  return(
+    <h2 class=" text-green-500 text-bold">
+      WElcome again {children}
+    </h2>
+  )
+}
---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,8 @@
+export default function Welcome(){
+  return (
+    <h2 class="text-2xl underline text-red-500">
+      Hello Welcome
+      <div> Hello here </div>
+    </h2>
+  )
+}
